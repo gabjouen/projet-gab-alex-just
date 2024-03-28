a94486698e551b8b0460866aa1bc792f6718ba52
gabjouen <gabinjouen2003@gmail.com>
2024-03-28 15:28:18 -0400

    création d'un script pour lire nos données combinées

---
+++ script lecture données.R
@@ -0,0 +1,4 @@
+#script pour ouvrir les données excel combinéees 
+#indiquer l'emplacer du document "combined_data.csv"
+#lire le fichier et le mettre dans un objet:
+data<-read.csv(file = "combined_data.csv", header = T)
