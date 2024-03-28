7afc1c047b157802d42a4750d7a51792bebe7f63
AlexRompre <roma1001@usherbrooke.ca>
2024-03-28 16:05:37 -0400

    trouver le nombre d'espèce dans data

--- script lecture données.R
+++ script lecture données.R
@@ -2,3 +2,6 @@
 #indiquer l'emplacer du document "combined_data.csv"
 #lire le fichier et le mettre dans un objet:
 data<-read.csv(file = "combined_data.csv", header = T)
+
+nom<-unique(data$nom_sci)
+length(nom)
